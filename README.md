fd45b078b843ac7d20bb95536af511db43ffd9b6
esraa-dardiery <83817520+esraa-dardiery@users.noreply.github.com>
2024-08-10 05:31:00 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-live-compiler-sexbvk
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/esraa-dardiery/angular-live-compiler-sexbvk)
---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import {
+  Compiler,
+  CompilerFactory,
+  COMPILER_OPTIONS,
+  NgModule,
+} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+ import { JitCompilerFactory } from '@angular/platform-browser-dynamic';
+
+@NgModule({
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  providers: [
+    { provide: COMPILER_OPTIONS, useValue: {}, multi: true },
+    {
+      provide: CompilerFactory,
+      useClass: JitCompilerFactory,
+      deps: [COMPILER_OPTIONS],
+    },
+    { provide: Compiler, useFactory: createCompiler, deps: [CompilerFactory] },
+  ],
+})
+export class AppModule {}
+
+export function createCompiler(compilerFactory: CompilerFactory) {
+  return compilerFactory.createCompiler();
+}
